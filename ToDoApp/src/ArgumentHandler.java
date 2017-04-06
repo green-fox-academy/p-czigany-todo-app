e6f8ca79e76c691562db2a81b24e6513d6d0f360
Peter Czigany <p.czigany@gmail.com>
2017-04-06 21:46:07 +0200

    Remove unused method

--- ToDoApp/src/ArgumentHandler.java
+++ ToDoApp/src/ArgumentHandler.java
@@ -9,17 +9,6 @@ public class ArgumentHandler {
     this.args = args;
   }
 
-  public boolean contains(String argument) {
-    if (args.length > 0) {
-      for (String arg : args) {
-        if (arg.startsWith("-") && arg.contains(argument)) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
   public String getTask() {
     String task = "";
     for (int i = 1; i < args.length; i++) {
